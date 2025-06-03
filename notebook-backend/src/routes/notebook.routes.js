f1d456c0625def1e60e89f1cbc00c9b7415c670b
Proyect <arieldiaz.sistemas@gmail.com>
2025-06-02 22:29:48 -0300

    server on notebook and prestamo - not finished

--- notebook-backend/src/routes/notebook.routes.js
+++ notebook-backend/src/routes/notebook.routes.js
@@ -15,3 +15,26 @@ router.put('/:id', notebookController.update);
 router.delete('/:id', notebookController.remove);
 
 module.exports = router;
+
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post('/notebooks', async (req, res) => {
+  try {
+    const nuevaNotebook = await prisma.notebook.create({
+      data: req.body
+    });
+    res.status(201).json(nuevaNotebook);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'No se pudo crear la notebook' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor en http://localhost:3000');
+});
