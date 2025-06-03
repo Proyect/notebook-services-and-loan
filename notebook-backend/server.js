b42f1bf352595ade25dce84f883f945510716121
Proyect <arieldiaz.sistemas@gmail.com>
2025-06-02 22:06:49 -0300

    implement user model

--- notebook-backend/server.js
+++ notebook-backend/server.js
@@ -8,3 +8,14 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
+
+//user routes
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// Obtener todos los usuarios
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+// Crear un nuevo usuario
